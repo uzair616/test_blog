9e2f667c6b1fc8f963f7a9d0d03916b6e2607579
uzair616 <ranauzair518@gmail.com>
2021-01-02 18:45:36 +0500

    last commit

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,6 @@
+class BlogController < ApplicationController
+  def index
+  end
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  #root 'blog#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'blog#index'
+  get 'about' , to: 'blog#about'
 end
