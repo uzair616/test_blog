2e63d36b93e4882410193de0c64da9b3b44d8a32
uzair616 <ranauzair518@gmail.com>
2021-01-04 19:32:22 +0500

    done with one to may association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to @article
