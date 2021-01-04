415651fe9bd97da7a8c74eb9a5b5e915ba3284a7
uzair616 <ranauzair518@gmail.com>
2021-01-04 21:05:08 +0500

    done with signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'blog#index'
   get 'about' , to: 'blog#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users , except: [:new]
 end
