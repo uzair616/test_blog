e6892e88aac83178d83c59a5bf2e251552f59418
uzair616 <ranauzair518@gmail.com>
2021-01-05 23:33:52 +0500

    done for today

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,6 @@
 class BlogController < ApplicationController
   def index
+    redirect_to articles_path if logged_in?
   end
   def about
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,19 @@
 class UsersController < ApplicationController
-
+  before_action :set_user , only: [:show,:edit,:update,:destroy]
+  before_action :require_user , only: [:edit,:update]
+  before_action :require_same_user , only: [:edit,:update,:destroy]
   def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 2)
   end
   def new
     @user = User.new
   end
   def edit
-    @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was updated successfully"
       redirect_to articles_path
@@ -25,15 +24,31 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Uzair Blog,#{@user.username} you are signed up"
       redirect_to articles_path
     else
       render 'new'
     end
   end
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:alert] = "Your Account and associated articles are deleted successfully"
+    redirect_to articles_path
+  end
 
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def require_same_user
+    if current_user!= @user && !current_user.admin?
+      flash[:alert] = "You can edit only your own profile"
+      redirect_to @user
+    end
   end
 end
